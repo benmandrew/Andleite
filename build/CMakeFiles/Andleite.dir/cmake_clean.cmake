0d8862617f1eb9e4e6e73471db0fdaae65a54fe2
Ben Andrew <benmandrew@gmail.com>
2020-03-01 21:21:32 +0000

    Fixed, and using CMake now!

---
+++ build/CMakeFiles/Andleite.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Andleite.dir/main.cpp.o"
+  "Andleite.pdb"
+  "Andleite"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Andleite.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
