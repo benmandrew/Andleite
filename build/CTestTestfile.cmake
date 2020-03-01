2c1fe0509c3740d9c68af7dde0fd7688f9f54862
Ben Andrew <benmandrew@gmail.com>
2020-03-01 21:23:02 +0000

    Ignoring CMake files

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ben/Desktop/Ändleite
-# Build directory: /home/ben/Desktop/Ändleite/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
